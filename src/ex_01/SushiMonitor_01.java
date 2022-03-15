72ebf8a86796952a983f8c6ddf659a5518c09493
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-03-15 13:33:47 +0100

    first approach to exercise 1

--- src/ex_01/SushiMonitor_01.java
+++ src/ex_01/SushiMonitor_01.java
@@ -5,12 +5,42 @@ import java.util.concurrent.locks.*;
 public class SushiMonitor_01 {
 
 	/* COMPLETE */
+	Lock accessGrantor = new ReentrantLock(false);
+	volatile int emptySeats = 5;
+	volatile boolean groupEating = false;
 
 	public void enter(int i) {
 		/* COMPLETE */
+		accessGrantor.lock();
+		System.out.println("----> Entering C(" + i + ")");
+
+		if (emptySeats == 0 && !groupEating) {
+			System.out.println("*** Possible group detected. I wait C(" + i + ") ");
+			groupEating = true;
+		}
+
+		if (groupEating) {
+			System.out.println("*** I'm told to wait for all free C(" + i + ")");
+		}
+
+		while (groupEating || emptySeats == 0) {
+			accessGrantor.unlock();
+			Thread.yield();
+			accessGrantor.lock();
+		}
+		emptySeats--;
+		System.out.println("+++ [free: " + emptySeats + "] I sit down C(" + i + ")");
 	}
 
 	public void exit(int i) {
 		/* COMPLETE */
+		System.out.println("---> now leaving [free: " + emptySeats + "] C(" + i + ")");
+		emptySeats++;
+
+		if (emptySeats == 5) {
+			groupEating = false;
+		}
+
+		accessGrantor.unlock();
 	}
 }
