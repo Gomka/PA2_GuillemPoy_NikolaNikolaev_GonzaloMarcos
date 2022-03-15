8571551c472bff5fe98096e997179c901bec5d13
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-03-15 12:24:38 +0100

    Practica de base, sin cambios

---
+++ src/ex_01/SushiMonitor_01.java
@@ -0,0 +1,18 @@
+package ex_01;
+
+import java.util.concurrent.locks.*;
+
+public class SushiMonitor_01 {
+	
+	
+	/* COMPLETE */
+	
+	
+	public void enter (int i) {
+		/* COMPLETE */
+	}
+	
+	public void exit (int i) {
+		/* COMPLETE */
+	}
+}
