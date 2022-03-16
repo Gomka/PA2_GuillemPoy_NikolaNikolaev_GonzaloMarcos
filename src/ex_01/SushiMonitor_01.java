a95b56ab17b00abb532aee6012bc50f8a5a588f0
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-03-16 20:35:43 +0100

    Creo que parte 1 done

--- src/ex_01/SushiMonitor_01.java
+++ src/ex_01/SushiMonitor_01.java
@@ -17,10 +17,16 @@ public class SushiMonitor_01 {
 		if (emptySeats == 0 && !groupEating) {
 			System.out.println("*** Possible group detected. I wait C(" + i + ") ");
 			groupEating = true;
+			accessGrantor.unlock();
+			Thread.yield();
+			accessGrantor.lock();
 		}
 
-		if (groupEating) {
+		if(groupEating) {
 			System.out.println("*** I'm told to wait for all free C(" + i + ")");
+				accessGrantor.unlock();
+				Thread.yield();
+				accessGrantor.lock();
 		}
 
 		while (groupEating || emptySeats == 0) {
@@ -28,19 +34,22 @@ public class SushiMonitor_01 {
 			Thread.yield();
 			accessGrantor.lock();
 		}
-		emptySeats--;
+
 		System.out.println("+++ [free: " + emptySeats + "] I sit down C(" + i + ")");
+		emptySeats--;
+		accessGrantor.unlock();
 	}
 
 	public void exit(int i) {
 		/* COMPLETE */
-		System.out.println("---> now leaving [free: " + emptySeats + "] C(" + i + ")");
+		accessGrantor.lock();
 		emptySeats++;
+		System.out.println("---> now leaving [free: " + emptySeats + "] C(" + i + ")");
 
 		if (emptySeats == 5) {
 			groupEating = false;
 		}
-
+		
 		accessGrantor.unlock();
 	}
 }
